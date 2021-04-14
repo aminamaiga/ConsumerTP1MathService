b62984dd40fd59e3a7b09e24ff5ab9a2e7de5e3c
aminata.maiga <veromaiga@gmail.com>
2021-04-14 18:36:11 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsumerTP1MathService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathServiceRef.MathService mathService = new MathServiceRef.MathService();
+
+            Console.WriteLine("Bonjour je consomme le web service MATH pour affectuer un ensemble d'opération \n");
+
+            Console.WriteLine(" Addition " + mathService.Add( 6, 9));
+            Console.WriteLine(" Soustraction " + mathService.Add(16, 9));
+            Console.WriteLine(" Division " + mathService.Add(26, 9));
+            Console.WriteLine(" Multiplication " + mathService.Add(3, 9));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# ConsumerTP1MathService
